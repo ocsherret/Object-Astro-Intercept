a66abb88ea49464a7890310e35f32a1f86cd266e
ocsherret <62727306+ocsherret@users.noreply.github.com>
2020-04-02 22:32:09 -0400

    File formatting
    Also have an issue with date for calculating sunrise/sunset  for an object in the future.

--- src/index.js
+++ src/index.js
@@ -4,87 +4,132 @@ const SunAstroTimes = require("./sunastrotimes");
 const MovingObject = require("./movingobject");
 const plane = new MovingObject();
 let timeNow = Date.now();
-console.log(new Date)
+console.log(new Date());
 let newLongitude = plane.getLongitude();
 let newLatitude = plane.getLatitude();
-const sun = new SunAstroTimes(timeNow, plane.getLatitude(), plane.getLongitude(), plane.getAltitudeFeet());
+const sun = new SunAstroTimes(
+    timeNow,
+    plane.getLatitude(),
+    plane.getLongitude(),
+    plane.getAltitudeFeet(),
+);
+const sun2 = new SunAstroTimes(
+    timeNow,
+    plane.getLatitude(),
+    plane.getLongitude(),
+    plane.getAltitudeFeet(),
+);
 const oneSecond = 1000;
 const oneMinute = 60000;
 const oneHour = 3600000;
 const oneDay = 86400000;
 
 
-
-function getPositionAndDirection(){
-
-}
-function calculateProjectedLatLong(distanceToSunset, timeToPoint){
-    //Purpose of this function is to get the projected Lat/Long using a great circle line.
-    //Then it uses the projected time of arrival of the object to recalculate the astro times.
+function calculateProjectedLatLong(distanceToSunset, timeToPoint) {
+    // Purpose of this function is to get the projected Lat/Long using a great circle line.
+    // Then it uses the projected time of arrival of the object to recalculate the astro times.
     let directionRad = deg.toRadians(plane.getTrueBearing());
     let latitudeRad = deg.toRadians(plane.getLatitude());
     let longitudeRad = deg.toRadians(plane.getLongitude());
-    
 
     newLatitude = Math.asin(
-                            Math.sin(latitudeRad) * Math.cos(distanceToSunset/3440)
-                                + Math.cos(latitudeRad) * Math.sin(distanceToSunset/3440) * Math.cos(directionRad)); 
+        Math.sin(latitudeRad) * Math.cos(distanceToSunset / 3440)
+            + Math.cos(latitudeRad) * Math.sin(distanceToSunset / 3440)
+            * Math.cos(directionRad),
+    );
 
-    
-    newLongitude = plane.getLongitude() + deg.toDegrees(Math.atan2(Math.sin(directionRad) * Math.sin(distanceToSunset/3440) * Math.cos(latitudeRad),
-                                    Math.cos(distanceToSunset/3440) - Math.sin(latitudeRad) * Math.sin(newLatitude)));
-    
-    
+    newLongitude = plane.getLongitude()
+        + deg.toDegrees(
+            Math.atan2(
+                Math.sin(directionRad) * Math.sin(distanceToSunset / 3440)
+                    * Math.cos(latitudeRad),
+                Math.cos(distanceToSunset / 3440) - Math.sin(latitudeRad)
+                    * Math.sin(newLatitude),
+            ),
+        );
+
     newLatitude = deg.toDegrees(newLatitude);
-    sun.reCalcTimes(timeToPoint, newLatitude, newLongitude, plane.getAltitudeFeet());
-    //console.log(newLatitude,newLongitude);
-    
-    
+    sun.reCalcTimes(
+        timeToPoint,
+        newLatitude,
+        newLongitude,
+        plane.getAltitudeFeet(),
+    );
+    // console.log(newLatitude,newLongitude);
 }
-function checkForSunsetIntercept(){    
+function checkForSunsetIntercept() {
     let timeToPoint = 0;
-    for(let i = 1; i <= plane.getSpeed()*12; i+=0.1){
-        timeToPoint = i/ plane.getSpeed() * oneHour + timeNow;
-        calculateProjectedLatLong(i , timeToPoint);
-        if(sun.getSunset() <= timeToPoint){
-            console.log("Sunset is:         " + new Date(sun.getSunset()).toString());
-            console.log("Intercept time is: " + new Date(timeToPoint).toString());
+    for (let i = 1; i <= plane.getSpeed() * 6; i += 0.1) {
+        timeToPoint = i / plane.getSpeed() * oneHour + timeNow;
+        calculateProjectedLatLong(i, timeToPoint);
+        if (sun.getSunset() <= timeToPoint) {
+            console.log(
+                "Sunset is:         " + new Date(sun.getSunset()).toString(),
+            );
+            console.log(
+                "Intercept time is: " + new Date(timeToPoint).toString(),
+            );
             console.log("Time now is:       " + new Date(timeNow).toString());
             console.log("Lat Long is:       " + newLatitude, newLongitude);
-            console.log("Distance to go is: " + i +"NM");
-            console.log("Time to go is:     " + (timeToPoint - timeNow) / oneHour + "Hrs");
+            console.log("Distance to go is: " + i + "NM");
+            console.log(
+                "Time to go is:     " + (timeToPoint - timeNow) / oneHour
+                    + "Hrs",
+            );
             break;
         }
     }
 }
-function checkForSunriseIntercept(){    
+function checkForSunriseIntercept() {
     let timeToPoint = 0;
-    for(let i = plane.getSpeed()*14; i >= 400; i--){
+    for (let i = plane.getSpeed() * 6; i >= 0 ; i -= 0.1) {
         timeToPoint = i / plane.getSpeed() * oneHour + timeNow;
-        calculateProjectedLatLong(i , timeToPoint);
-        
-        if(sun.getSunrise() >= timeToPoint){
-            console.log("Sunrise is:            "+new Date(sun.getSunrise()).toString());
-            console.log("Intercept time is:     "+ new Date(timeToPoint).toString());
-            console.log("Time now is:           " + new Date(timeNow).toString());
-            console.log("Lat Long is:           "+ newLatitude, newLongitude);
-            console.log("Distance to go is:     " + i +"NM");
-            console.log("Time to go is:         " + (timeToPoint - timeNow) / oneHour + "Hrs");
-            console.log(sun.getSunrise());
-            console.log(timeToPoint);
-            console.log(sun.getSunrise() >= timeToPoint);
+
+        calculateProjectedLatLong(i, timeToPoint);
+        console.log(new Date(timeToPoint).toString());
+        console.log(new Date(sun.getSunrise()).toString());
+
+        if (sun.getSunrise() >= timeToPoint) {
+            console.log("||------------------------------------------------------------------||")
+            console.log(
+                "Sunrise is:            "
+                    + new Date(sun.getSunrise()).toString(),
+            );
+            console.log(
+                "Intercept time is:     " + new Date(timeToPoint).toString(),
+            );
+            console.log(
+                "Time now is:           " + new Date(timeNow).toString(),
+            );
+            console.log("Lat Long is:           " + newLatitude.toFixed(3), newLongitude.toFixed(3));
+            console.log("Distance to go is:     " + i.toFixed(1) + "NM");
+            console.log(
+                "Time to go is:         " + ((timeToPoint - timeNow) / oneHour).toFixed(2)
+                    + "Hrs",
+            );
             break;
-        }   
-        
+        }else{
+            console.log("No sunrise expected in the next six hours.");
+            break;
+        }
     }
-    //console.log("No expected sunrise in :" + 15000/speed + "Hrs.")
+    // console.log("No expected sunrise in :" + 15000/speed + "Hrs.")
 }
-function check(){
+function check() {
     timeNow = Date.now();
-    checkForSunsetIntercept();
+    sun2.reCalcTimes(
+        timeNow,
+        plane.getLatitude(),
+        plane.getLongitude(),
+        plane.getAltitudeFeet(),
+    );
+    if(timeNow > sun2.getSunrise() && timeNow < sun2.getSunset()){
+        checkForSunsetIntercept();
+    } else {
+        checkForSunriseIntercept();
+    }
+
     plane.move();
-    //checkForSunriseIntercept();
+    
 }
 const myHeartBeat = setInterval(check, 5000);
-
- 
--- src/movingobject.js
+++ src/movingobject.js
@@ -1,76 +1,91 @@
 const Degrees = require("./degrees");
 const deg = new Degrees();
 
-class MovingObject{
-    constructor(){
-        this.trueBearing = 70;
+class MovingObject {
+    constructor() {
+        this.trueBearing = 90;
         this.latitude = 43;
-        this.longitude = -79;
+        this.longitude = 0;
         this.altitudeFeet = 30000;
-        this.speed = 1200;
+        this.speed = 600;
     }
-    getTrueBearing(){
+    getTrueBearing() {
         return this.trueBearing;
     }
-    setTrueBearing(trueBearing){
+    setTrueBearing(trueBearing) {
         this.trueBearing = trueBearing;
     }
-    getLatitude(){
+    getLatitude() {
         return this.latitude;
     }
-    setLatitude(latitude){
+    setLatitude(latitude) {
         this.latitude = latitude;
     }
-    getLongitude(){
+    getLongitude() {
         return this.longitude;
     }
-    setLongitude(longitude){
+    setLongitude(longitude) {
         this.longitude = longitude;
     }
-    getAltitudeFeet(){
-        return this.altitudeFeet
+    getAltitudeFeet() {
+        return this.altitudeFeet;
     }
-    setAltitudeFeet(altitudeFeet){
+    setAltitudeFeet(altitudeFeet) {
         this.altitudeFeet = altitudeFeet;
     }
-    getSpeed(){
+    getSpeed() {
         return this.speed;
     }
-    setSpeed(speed){
+    setSpeed(speed) {
         this.speed = speed;
     }
-    move(){
-    let directionRad = deg.toRadians(this.trueBearing);
-    let latitudeRad = deg.toRadians(this.latitude);
-    let longitudeRad = deg.toRadians(this.longitude);
-    let newLatitude = this.latitude;
-    let newLongitude = this.longitude;
-    let newBearing = this.trueBearing;
-    let newBearingY = null;
-    let newBearingX = null;
-    let funcDistance = this.speed * (5000/3600000);
+    move() {
+        let directionRad = deg.toRadians(this.trueBearing);
+        let latitudeRad = deg.toRadians(this.latitude);
+        let longitudeRad = deg.toRadians(this.longitude);
+        let newLatitude = this.latitude;
+        let newLongitude = this.longitude;
+        let newBearing = this.trueBearing;
+        let newBearingY = null;
+        let newBearingX = null;
+        let funcDistance = this.speed * (5000 / 3600000);
 
-    newLatitude = Math.asin(
-                            Math.sin(latitudeRad) * Math.cos(funcDistance/3440)
-                                + Math.cos(latitudeRad) * Math.sin(funcDistance/3440) * Math.cos(directionRad)); 
+        newLatitude = Math.asin(
+            Math.sin(latitudeRad) * Math.cos(funcDistance / 3440)
+                + Math.cos(latitudeRad) * Math.sin(funcDistance / 3440)
+                * Math.cos(directionRad),
+        );
 
-    
-    newLongitude = this.longitude + deg.toDegrees(Math.atan2(Math.sin(directionRad) * Math.sin(funcDistance/3440) * Math.cos(latitudeRad),
-                                    Math.cos(funcDistance/3440) - Math.sin(latitudeRad) * Math.sin(newLatitude)));
-    
-    
-    newBearingY = Math.sin(longitudeRad - deg.toRadians(newLongitude)) * Math.cos(latitudeRad);
-    newBearingX = Math.cos(newLatitude) * Math.sin(latitudeRad) - Math.sin(newLatitude)*Math.cos(latitudeRad)*Math.cos(longitudeRad-deg.toRadians(newLongitude))
-    newBearing = deg.toDegrees(Math.atan2(newBearingY, newBearingX))
-    newBearing = (newBearing + 180) % 360;
-    //Conversion to here as these variables are needed in Radians.
-    newLatitude = deg.toDegrees(newLatitude);
-    
-    this.trueBearing = newBearing;
-    this.latitude = newLatitude;
-    this.longitude = newLongitude;
-    console.log("Current Lat/Long:          " + this.latitude.toFixed(2),this.longitude.toFixed(2));
-    console.log("Current True Bearing:      "+ this.trueBearing.toFixed(2));    //console.log(newLatitude,newLongitude);
+        newLongitude = this.longitude
+            + deg.toDegrees(
+                Math.atan2(
+                    Math.sin(directionRad) * Math.sin(funcDistance / 3440)
+                        * Math.cos(latitudeRad),
+                    Math.cos(funcDistance / 3440) - Math.sin(latitudeRad)
+                        * Math.sin(newLatitude),
+                ),
+            );
+
+        newBearingY = Math.sin(longitudeRad - deg.toRadians(newLongitude))
+            * Math.cos(latitudeRad);
+        newBearingX = Math.cos(newLatitude) * Math.sin(latitudeRad)
+            - Math.sin(newLatitude) * Math.cos(latitudeRad)
+            * Math.cos(longitudeRad - deg.toRadians(newLongitude));
+        newBearing = deg.toDegrees(Math.atan2(newBearingY, newBearingX));
+        newBearing = (newBearing + 180) % 360;
+        // Conversion to here as these variables are needed in Radians.
+        newLatitude = deg.toDegrees(newLatitude);
+
+        this.trueBearing = newBearing;
+        this.latitude = newLatitude;
+        this.longitude = newLongitude;
+        console.log(
+            "Current Lat/Long:          " + this.latitude.toFixed(2),
+            this.longitude.toFixed(2),
+        );
+        console.log(
+            "Current True Bearing:      " + this.trueBearing.toFixed(2),
+        ); // console.log(newLatitude,newLongitude);
     }
 }
 module.exports = MovingObject;
